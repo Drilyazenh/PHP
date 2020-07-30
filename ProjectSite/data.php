50222b35171b645783e3b1fc87f9985ad50c8827
Drilyazenh <67309004+Drilyazenh@users.noreply.github.com>
2020-07-30 21:21:51 +0800

    Выгрузка лотов из бд

--- ProjectSite/data.php
+++ ProjectSite/data.php
@@ -47,49 +47,26 @@ if (isset($_COOKIE["history"])) {
 }
 setcookie($name,$value, $expire, $path);
 
+if (!$link) {
+    $error = mysqli_connect_error();
+    print ("Ошибка");
+} else{
+    $sql = 'SELECT * FROM lot';
+    $sqlForCat = 'SELECT * FROM category';
 
+    $result = mysqli_query($link, $sql);
+    if ($result) {
 
-
+        $adArr = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+    else {
+        print ("Ошибка");
+        mysqli_error($link);
+    }
+}
 
 $categories = ["Доски и лыжи","Крепления","Ботинки","Одежда","Инструменты","Разное"];
-$adArr = [
-    [
-        'title'=>'2014 Rossignol District Snowboard',
-        'category'=>$categories[0],
-        'price'=>10999,
-        'path'=>'img/lot-1.jpg'
-    ],
-    [
-        'title'=>'DC Ply Mens 2016/2017 Snowboard',
-        'category'=>$categories[0],
-        'price'=>159999,
-        'path'=>'img/lot-2.jpg'
-    ],
-    [
-        'title'=>'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category'=>$categories[1],
-        'price'=>8000,
-        'path'=>'img/lot-3.jpg'
-    ],
-    [
-        'title'=>'Ботинки для сноуборда DC Mutiny Charocal',
-        'category'=>$categories[2],
-        'price'=>10999,
-        'path'=>'img/lot-4.jpg'
-    ],
-    [
-        'title'=>'Куртка для сноуборда DC Mutiny Charocal',
-        'category'=>$categories[3],
-        'price'=>7500,
-        'path'=>'img/lot-5.jpg'
-    ],
-    [
-        'title'=>'Маска Oakley Canopy',
-        'category'=>$categories[5],
-        'price'=>5400,
-        'path'=>'img/lot-6.jpg'
-    ]
-];
+
 
 
 // ставки пользователей, которыми надо заполнить таблицу
